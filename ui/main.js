eaa4904dc2f790a2761d80a2843b33b60e6cde25
bushsk <bushra.sk@siesgst.ac.in>
2017-02-10 15:41:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,8 @@
 console.log('Loaded!');
+var b=document.getElementById('b1');
+var c=0;
+b.onClick= function(){
+    c=c+1;
+    var span=document.getElementById('count');
+    span.innerHTML=c.toString();
+}
