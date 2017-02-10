0bc90a450e15211ec4bd6581079608f1bb9dd08f
bushsk <bushra.sk@siesgst.ac.in>
2017-02-10 15:41:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 var b=document.getElementById('b1');
 var c=0;
-b.onClick= function(){
+b.onclick= function(){
     c=c+1;
     var span=document.getElementById('count');
     span.innerHTML=c.toString();
